e937a96a5f8b1c1c27ea98318f9d261a1c1ce9b1
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-11-08 21:45:46 +0300

    Fixe bug in this test

--- 4A-goodLuck/main.cpp
+++ 4A-goodLuck/main.cpp
@@ -7,7 +7,7 @@ int main()
     int a;
     cin >> a;
 
-    if (a % 5 == 0 && (a > 999 && a < 9999) || (a < -999 && a > -9999)) {
+    if (a % 5 == 0 && ((a > 999 && a < 9999) || (a < -999 && a > -9999))) {
         cout << "SUCCESS" << endl;
     } else {
         cout << "FAILURE" << endl;
